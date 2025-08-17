cc387ca1362f8c56cf47b42a6d4e2b15ddfa7be8
PULAK-SN <pulak.official22@gmail.com>
2025-08-17 21:42:35 +0530

    auto-complete added

--- auto-complete/src/App.js
+++ auto-complete/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [results, setResults] = useState("");
+  const [input, setInput] = useState("");
+  const [showResults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
+
+  const fetchData = async () => {
+    if (cache[input]) {
+      // console.log("cache retuen " + input);
+      setResults(cache[input]);
+      return;
+    }
+    // console.log("API_CALL " + input);
+    const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
+    const json = await data.json();
+    setResults(json?.recipes);
+    setCache((prev) => ({ ...prev, [input]: json?.recipes }));
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(fetchData, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Auto Compulete</h1>
+      <div>
+        <input
+          className="app_input"
+          type="text"
+          value={input}
+          onFocus={() => setShowResults(true)}
+          onBlur={() => setShowResults(false)}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {showResults && results.length > 0 && (
+          <div className="results_container">
+            {results?.map((item) => (
+              <span className="results" key={item.id}>
+                {item.name}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
